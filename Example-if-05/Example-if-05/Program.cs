b3e45e4e984ed1f681d843c20197f24496ee15b9
mahdi <mahdihasanvandi2540@gmail.com>
2025-05-26 20:20:21 +0330

    if-05 added

---
+++ Example-if-05/Example-if-05/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_if_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // برنامه بنویسید که 4 عدد بگیرد و اولین عدد زوج میان انها را چاپ کند
+   
+            Console.Write("Please enter number1:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please enter number2:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please enter number3:");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please enter number4:");
+            int num4 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 % 2 == 0)
+            {
+                Console.WriteLine(num1);
+            }
+            else if (num2 % 2 == 0)
+            {
+                Console.WriteLine(num2);
+            }
+            else if (num3 % 2 == 0)
+            {
+                Console.WriteLine(num3);
+            }
+            else if (num4 % 2 == 0)
+            {
+                Console.WriteLine(num4);
+            }
+            else
+            {
+                Console.WriteLine("all of the numbers are odd");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
