947ed6e35afe8173eceedc89827349e2d7b523ee
mahdi <mahdihasanvandi2540@gmail.com>
2025-05-26 20:04:49 +0330

    example-if-03 added

---
+++ Example-if-03/Example-if-03/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_if_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //گرفتن دو عدد از ورودی به دست اوردن تقسیم و باقی مانده عدد بزرگتر
+            Console.Write("please enter your number1:");
+            int num01 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("please enter your number2:");
+            int num02 = Convert.ToInt32(Console.ReadLine());
+
+            int max = num01;
+
+            if (num02 > num01)
+            {
+                max = num02;
+            }
+
+            Console.WriteLine($"Div = {max / 10}");
+            Console.WriteLine($"rem = {max % 10}");
+
+            Console.ReadLine();
+        }
+    }
+}
