b9f1090fffcdb252162bccb492c2377f8c882e98
mahdi <mahdihasanvandi2540@gmail.com>
2025-05-27 11:59:31 +0330

    added if-06 if-07

---
+++ Example-if-06/Example-if-06/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_if_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //چهار عدد بگیرد جایگاه عدد بزرگتر و کوچک تر در جفت اول و دوم مشابه هست؟
+            Console.Write("please enter your number1:");
+            int num01 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("please enter your number2:");
+            int num02 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please enter your number3:");
+            int num03 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please enter your number4:");
+            int num04 = Convert.ToInt32(Console.ReadLine());
+
+            if (num01 > num02 && num03 > num04 || num01 < num02 && num03 < num04)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ Example-if-07/Example-if-07/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_if_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //برنامه دو عدد میگیرد اگر هر دو بر 3 یا 7 بخش پذیر بود پیغام 1 در غیر این صورت پیغام 2 چام میشود
+            Console.Write("please enter your number1:");
+            int num01 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("please enter your number2:");
+            int num02 = Convert.ToInt32(Console.ReadLine());
+
+            if (num01 % 3 == 0 && num02 % 3 == 0 || num01 % 7 == 0 && num02 % 7 == 0)
+            {
+                Console.WriteLine("1");
+            }
+            else
+            {
+                Console.WriteLine("2");
+            }
+        }
+    }
+}
