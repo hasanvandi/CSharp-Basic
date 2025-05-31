d9a67c851c734437c34545922a21f36bdd47eb3b
mahdi <mahdihasanvandi2540@gmail.com>
2025-05-31 11:00:38 +0330

    example-if-08 and 09 added

---
+++ Example-if-08/Example-if-08/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_if_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //نمره دریافت کن زیر ده بود ضربش کن در عدد مورد نظر اگر بین 9 و 10 بود تبدیل به 10 کن و نمایشش بده
+            Console.WriteLine("enter your score:");
+            double score = Convert.ToInt32(Console.ReadLine());
+
+
+
+            if (score < 10)
+            {
+                score = score * 1.2;
+                if (score > 9 && score < 10)
+                {
+                    score = 10;
+                }
+            }
+
+            Console.WriteLine(score);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Example-if-09/Example-if-09/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_if_09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //برنامه عددی میگیرد اگر زوج بود عددی دیگر میگیرد و جمع انهارا نمایش میدهد
+            Console.Write("Please enter your number:");
+            int num01 = Convert.ToInt32(Console.ReadLine());
+
+            int sum = num01;
+
+            if (num01 % 2 == 0)
+            {
+                Console.Write("Please enter your number next:");
+                int num02 = Convert.ToInt32(Console.ReadLine());
+                sum += num02;
+            }
+           
+                Console.WriteLine(sum);
+            
+            Console.ReadLine();
+        }
+    }
+}
