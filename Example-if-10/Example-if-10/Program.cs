3a32d1c89f71c0bb57cb464f552e0f7ebec07047
mahdi <mahdihasanvandi2540@gmail.com>
2025-06-01 12:44:51 +0330

    if-10 and if-11 added

---
+++ Example-if-10/Example-if-10/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_if_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //برنامه سه عدد گرفته و اعداد فرد رو جمع میکند
+            Console.Write("please enter number1:");
+            int num1 =int.Parse(Console.ReadLine());
+            Console.Write("please enter number2:");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.Write("please enter number3:");
+            int num3 = int.Parse(Console.ReadLine());
+
+            int count = 0;
+            if(num1 % 2 == 1)
+            {
+                count += num1;
+            }
+            if(num2 % 2 == 1)
+            {
+                count += num2;
+            }
+            if (num3 % 2 == 1)
+            {
+                count += num3;
+            }
+            Console.WriteLine(count);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Example-if-11/Example-if-11/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_if_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //دو عدد میگیریم حاصل ده برابر عدد بزرگتر منهای توان دوم عدد کوچک تر را نمایش میدهیم
+            Console.WriteLine("please enter number1:");
+            int num01 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("please enter number2:");
+            int num02 = Convert.ToInt32(Console.ReadLine());
+
+            int max = num01;
+            int min = num01;
+            if (num02 > max)
+            {
+                max = num02;
+            }
+            else if (num02 < min)
+            {
+                min = num02;
+            }
+            int result = (10 * max) - (min * min);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
