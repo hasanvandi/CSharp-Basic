4883c191ceb40291d6db66881dd7579d1a6d640a
mahdi <mahdihasanvandi2540@gmail.com>
2025-06-08 17:35:05 +0330

    deleted README for If-Example and Example-if-20 added

---
+++ Example-if-20/Example-if-20/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_if_20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //برنامه سه تا عدد میگیرد عددی که بزرگترین دهگان دارد یکانش از دهگان بزرگ تر است؟
+            Console.Write("Please enter number1:");
+            int num01 = int.Parse(Console.ReadLine());
+            Console.Write("Please enter number2:");
+            int num02 = int.Parse(Console.ReadLine());
+            Console.Write("Please enter number3:");
+            int num03 = int.Parse(Console.ReadLine());
+
+            int d1 = num01 % 10;
+            int d2 = num01 / 10;
+
+            int d3 = num02 % 10;
+            int d4 = num02 / 10;
+
+            int d5 = num03 % 10;
+            int d6 = num03 / 10;
+
+            int maxD = d2;
+            if(d4 > maxD)
+            {
+                maxD = d4;
+                if(maxD < d3)
+                {
+                    Console.WriteLine("yes");
+                }
+                else
+                {
+                    Console.WriteLine("no");
+                }
+            }
+            if (d6 > maxD)
+            {
+                maxD = d6;
+                if (maxD < d1)
+                {
+                    Console.WriteLine("yes");
+                }
+                else
+                {
+                   Console.WriteLine("no");
+                }
+            }
+            if(d2 < d1)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+               Console.WriteLine("no");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
